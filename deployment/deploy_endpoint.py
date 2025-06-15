39ce2b99c321f48c49ff9d4c60ea6155ec78e256
rvishakhs <rvishakhs@gmail.com>
2025-06-15 13:13:51 +0100

    deployment sucessufull

--- deployment/deploy_endpoint.py
+++ deployment/deploy_endpoint.py
@@ -2,41 +2,40 @@ from sagemaker.pytorch.model import PyTorchModel
 import sagemaker
 import logging
 
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 def deploy_endpoint():
+
     logger.info("Starting SageMaker session...")
     sagemaker.Session()
-    role ='arn:aws:iam::973787923108:role/feedback-analysis-endpoint-role'
+    role = "arn:aws:iam::973787923108:role/feedback-analysis-endpoint-role"
 
-    model_uri = 's3://feedback-analysis-saas/inference/model.tar.gz'
+    model_uri = "s3://feedback-analysis-saas/inference/model.tar.gz"
 
     logger.info("Creating PyTorchModel...")
     model = PyTorchModel(
         model_data=model_uri,
         role=role,
-        entry_point='inference.py',
-        framework_version="2.5",
-        py_version='py311',
-        source_dir='deploy',
-        name='feedback-analysis-model',
+        framework_version="2.5.1",
+        py_version="py311",
+        entry_point="inference.py",
+        source_dir="deploy",
+        name="sentiment-analysis-model",
     )
-    
     logger.info("Deploying model to endpoint 'feedback-analysis-endpoint'...")
     predictor = model.deploy(
         initial_instance_count=1,
-        instance_type='ml.g5.xlarge',
-        endpoint_name='feedback-analysis-endpoint',
+        instance_type="ml.g5.xlarge",
+        endpoint_name="sentiment-analysis-endpoint",
     )
 
     logger.info("Model deployed successfully.")
     return predictor
 
+
 if __name__ == "__main__":
     predictor = deploy_endpoint()
     print("Endpoint deployment script executed successfully.")
-
-
     
